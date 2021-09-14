2f1a5678c6c5503ac2331e88a4a91bf0e2a2b317
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-09-14 17:43:32 +0300

    Import Models component.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import 'aos/dist/aos.css'
 
 /* COMPONENTS */
 import Header from './Components/Header'
+import Models from './Components/Models'
 AOS.init()
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Models></Models>
     </div>
   )
 }
