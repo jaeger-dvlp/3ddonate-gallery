602cb7513cf97bc2de4bfaabc4088a664986b335
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-09-14 18:00:08 +0300

    Change scene lights.

--- src/Components/ModelViewer.js
+++ src/Components/ModelViewer.js
@@ -11,21 +11,24 @@ export class ModelViewer extends Component {
       <div className="model-viewer-cont col-6 col-xl-4 col-lg-4 p-2">
         <div className="model-viewer mx-auto col-12 p-2">
           <STLViewer
-            className="mx-auto col-12"
+            className="mx-auto"
+            width={150}
             height={200}
             modelColor="white"
             backgroundColor="white"
             model={'./3dModels/' + this.props.model + '.stl'}
             rotate={true}
             rotationSpeeds={[0.0, 0.005, 0.0]}
+            lights={[1, 1, 1]}
+            lightColor="white"
             cameraY={0}
             cameraX={-300}
           ></STLViewer>
-          <div className="model-header col-12 text-center">Poşet Tutucu</div>
-          <div className="model-desc col-12 text-justify">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown.
+          <div className="model-header col-12 text-center p-1 p-xl-3 p-lg-3">
+            {this.props.name}
+          </div>
+          <div className="model-desc col-12 text-justify p-1 p-xl-3 p-lg-3">
+            {this.props.desc}
           </div>
         </div>
       </div>
