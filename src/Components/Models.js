63c3855a840489891c5efa3a43f0be78cb11c739
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-09-14 17:44:10 +0300

    Create & configure STLViewer.

--- src/Components/Models.js
+++ src/Components/Models.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react'
+import ModelViewer from './ModelViewer'
 
 export class Models extends Component {
   render() {
-    return <div></div>
+    return (
+      <div className="container mt-5">
+        <ModelViewer model="posetTutucu"></ModelViewer>
+      </div>
+    )
   }
 }
 
