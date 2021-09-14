c3ab7f8ce0434c5398dbcafb3cdc87170693fe74
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-09-14 18:00:09 +0300

    Edit responsive.

--- src/Components/Models.js
+++ src/Components/Models.js
@@ -5,7 +5,32 @@ export class Models extends Component {
   render() {
     return (
       <div className="container mt-5">
-        <ModelViewer model="posetTutucu"></ModelViewer>
+        <div className=" col-12 d-flex row m-0">
+          <ModelViewer
+            model="posetTutucu"
+            name="Poşet Tutucu"
+            desc="
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown."
+          ></ModelViewer>
+          <ModelViewer
+            model="posetTutucu"
+            name="Poşet Tutucu"
+            desc="
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown."
+          ></ModelViewer>
+          <ModelViewer
+            model="posetTutucu"
+            name="Poşet Tutucu"
+            desc="
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown."
+          ></ModelViewer>
+        </div>
       </div>
     )
   }
