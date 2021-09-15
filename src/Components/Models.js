310d50a8ef426d843ed3387e713471a87b179489
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-09-15 09:47:32 +0300

    Get data from json and convert.

--- src/Components/Models.js
+++ src/Components/Models.js
@@ -1,87 +1,37 @@
 import React, {Component} from 'react'
 import ModelViewer from './ModelViewer'
+import json3dModels from '../_3dmodels.json'
 
 export class Models extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ready3dModels: []
+    }
+  }
+  componentDidMount = () => {
+    this.renderModels()
+  }
+
+  renderModels = async () => {
+    const _3dModels = JSON.parse(JSON.stringify(json3dModels))
+    this.setState({ready3dModels: _3dModels})
+  }
+
   render() {
-    const _3dModels = [
-      {
-        model: 'posetTutucu',
-        name: 'Poşet Tutucu',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown.`
-      },
-      {
-        model: 'adaptifAnahtar',
-        name: 'Adaptif Anahtar',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown.`
-      },
-      {
-        model: 'cimbiz',
-        name: 'Cımbız',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown.`
-      },
-      {
-        model: 'fonksiyonelAdaptifUrun',
-        name: 'Fonksiyonel Adaptif Ürün',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown.`
-      },
-      {
-        model: 'corapGiymeAparati',
-        name: 'Çorap Giyme Aparatı',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown.`
-      },
-      {
-        model: 'kasikCatalAparati',
-        name: 'Kaşık-Çatal Aparatı',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown.`
-      },
-      {
-        model: 'fermuarCekmeAparati',
-        name: 'Fermuar Çekme Aparatı',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown.`
-      },
-      {
-        model: 'atmKartTutucu',
-        name: 'ATM Kart Tutucu',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown.`
-      },
-      {
-        model: 'genisYemekAksesuari',
-        name: 'Geniş Yemek Aksesuarı',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown.`
-      }
-    ]
+    const models = this.state.ready3dModels.map((object, id) => {
+      return (
+        <ModelViewer
+          key={id}
+          model={object.model}
+          name={object.name}
+          desc={object.desc}
+        ></ModelViewer>
+      )
+    })
     return (
       <div className="container mt-5 mb-5">
-        <div className=" col-12 d-flex row m-0">
-          {_3dModels.map((object, id) => {
-            return (
-              <ModelViewer
-                key={id}
-                model={object.model}
-                name={object.name}
-                desc={object.desc}
-              ></ModelViewer>
-            )
-          })}
-        </div>
+        <div className=" col-12 d-flex row m-0">{models}</div>
       </div>
     )
   }
